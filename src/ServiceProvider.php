1a0120fd66b4114929516b8ad728ced897a5b5cc
Sander Muller <github@scode.nl>
2025-01-29 20:07:27 +0100

    Add initial Laravel support

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Collectiq\SolanaPhpSdk;
+
+final class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/solana-php-sdk.php' => config_path('solana-php-sdk.php'),
+        ]);
+
+        Bootstrap::createContainer(
+            file_exists(config_path('solana-php-sdk.php'))
+                ? config_path('solana-php-sdk.php')
+                : __DIR__.'/../config/solana-php-sdk.php'
+        );
+    }
+}
