dbdf3d7a5707b7b2f31d6b6a225c8847596e3fb0
Sander Muller <github@scode.nl>
2025-02-12 18:11:30 +0100

    Update IsProgram.php

--- src/Programs/IsProgram.php
+++ src/Programs/IsProgram.php
@@ -4,11 +4,18 @@ namespace Collectiq\SolanaPhpSdk\Programs;
 
 use Collectiq\SolanaPhpSdk\SolanaRpcClient;
 use Illuminate\Container\Container;
+use ReflectionProperty;
 
 trait IsProgram
 {
     private SolanaRpcClient $client {
         get {
+            $reflectionProperty = new ReflectionProperty($this, 'client');
+
+            if ($reflectionProperty->isInitialized($this)) {
+                return $this->client;
+            }
+
             return Container::getInstance()->get(SolanaRpcClient::class);
         }
     }
